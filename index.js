2494cfa3802cebb749c8bf57cfa17630c360ea00
tomekk06 <31384112+tomekk2006@users.noreply.github.com>
2025-03-12 13:06:10 +0000

    added options to parser

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-class Block {
+class ParseBlock {
     static heading(block) {
         const childrenElements = parseBlocks(block.children, {join_str:""});
         const htmlString = `<h${block.level}>${childrenElements}</h${block.level}>`;
@@ -9,8 +9,14 @@ class Block {
         const htmlString = `<p>${childrenElements}</p>`;
         return htmlString; // example output: <p>Hello world!</p>
     }
-    static text(block) {
-        let text = encodeHtml(block.text)
+    static text(block, options={}) {
+        let encode_text = true;
+        if ("encode_text" in options) encode_text = options.encode_text;
+
+        let text;
+        if (encode_text) text = encodeHtml(block.text);
+        else text = block.text;
+
         if (block.bold) text = "<b>"+text+"</b>";
         if (block.underline) text = "<u>"+text+"</u>";
         if (block.strikethrough) text = "<s>"+text+"</s>";
@@ -23,14 +29,19 @@ class Block {
         const htmlString = `<a href="${block.url}">${childrenElements}</a>`;
         return htmlString; // example output: <a href="/page">Hello world!</a>
     }
-    static list(block) {
+    static list(block, options={}) {
+        let indent_size = 4;
+        if ("indent_size" in options) indent_size = options.indent_size;
+        let join_str = "\n";
+        if ("join_str" in options) join_str = options.join_str
+
         const listItems = new Array();
         block.children.forEach((item) => {
             if (item.type === "list-item") {
-                listItems.push("\n    <li>"+parseBlocks(item.children)+"</li>");
+                listItems.push(join_str+" ".repeat(indent_size)+"<li>"+parseBlocks(item.children)+"</li>");
             }
         });
-        const htmlString = listItems.join("")+"\n";
+        const htmlString = listItems.join("")+join_str;
         if (block.format === "ordered") return "<ol>"+htmlString+"</ol>";
         if (block.format === "unordered") return "<ul>"+htmlString+"</ul>";
     }
@@ -49,44 +60,45 @@ class Block {
         return htmlString; 
     }
 }
-
+// options:
+//     join_str: string
+//     encode_text: boolean
+//     indent_size: integer
 function parseBlocks(blocks, options={}) {
-    let htmlElements = new Array();
+    let join_str = "\n";
+    if ("join_str" in options) join_str = options.join_str;
+
+    const htmlElements = new Array();
     blocks.forEach((block) => {
         switch (block.type) {
             case "heading":
-                htmlElements.push(Block.heading(block));
+                htmlElements.push(ParseBlock.heading(block));
                 break;
             case "paragraph":
-                htmlElements.push(Block.paragraph(block));
+                htmlElements.push(ParseBlock.paragraph(block));
                 break;
             case "text":
-                htmlElements.push(Block.text(block));
+                if ("encode_text" in options) htmlElements.push(ParseBlock.text(block, options));
+                else htmlElements.push(ParseBlock.text(block));
                 break;
             case "link":
-                htmlElements.push(Block.link(block));
+                htmlElements.push(ParseBlock.link(block));
                 break;
             case "list":
-                htmlElements.push(Block.list(block));
+                htmlElements.push(ParseBlock.list(block, options));
                 break;
             case "code":
-                htmlElements.push(Block.codeBlock(block));
+                htmlElements.push(ParseBlock.codeBlock(block));
                 break;
             case "image":
-                htmlElements.push(Block.image(block));
+                htmlElements.push(ParseBlock.image(block));
                 break;
             case "quote":
-                htmlElements.push(Block.quote(block));
+                htmlElements.push(ParseBlock.quote(block));
                 break;
         }
     });
-    let htmlString;
-    if ("join_str" in options) {
-        htmlString = htmlElements.join(options.join_str);
-    }
-    else {
-        htmlString = htmlElements.join("\n");
-    }
+    const htmlString = htmlElements.join(join_str);
     return htmlString;
 }
 
